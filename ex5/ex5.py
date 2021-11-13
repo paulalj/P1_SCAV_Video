1f1f30b119bf2f3974f0f91edcae944b5727b1bc
Paula Liñán Jurado <paula.linan01@estudiant.upf.edu>
2021-11-13 11:09:20 +0100

    Update ex5.py

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -1,18 +1,15 @@
 import os, sys
 import cv2
 import numpy as np
-#from PIL import Image
 
 def main():
     image = 'Ona_tortu.png'
     img = cv2.imread(image, 0) #Llegim la imatge en blanc i negre
     img_num = np.float32(img) #La passem a floats
     num_dct = cv2.dct(img_num) #Fem la DCT
-    #img_dct = Image.fromarray(num_dct) #La tornem a convertir a imatge
     cv2.imwrite('Ona_tortu_DCT.png', num_dct) #Guardem la imatge
 
     num_idct = cv2.idct(num_dct) #Fem la inversa, és a dir, decodifiquem
-    #img_idct = Image.fromarray(num_idct) #La tornem a convertir a imatge
     cv2.imwrite('Ona_tortu_IDCT.png', num_idct) #Guardem la imatge
 
 if __name__=="__main__":
